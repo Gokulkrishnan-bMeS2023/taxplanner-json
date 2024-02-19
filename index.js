2883a486e8da1f7b411c51c7da5f43d5c67b65e3
Gokulkrishnan47 <gokulkrishnan20092002@gmail.com>
2024-02-19 11:30:34 +0530

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); //importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleweres=jsonServer.defaults();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //chose port from like 8080 , 3001
 
 server.use(middleweres);
 server.use(router);
 
-server.listen(port)
+server.listen(port);
